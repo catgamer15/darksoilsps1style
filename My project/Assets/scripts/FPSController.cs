add204fe471033be2acdfc44be225595c0567465
catgamer15 <112816053+catgamer15@users.noreply.github.com>
2025-03-29 15:41:33 +0300

    newScript

--- My project/Assets/scripts/FPSController.cs
+++ My project/Assets/scripts/FPSController.cs
@@ -8,6 +8,14 @@ using Cinemachine;
 
 public class FPSController : MonoBehaviour
 {
+    [Header("Movement")]
+    [SerializeField] private float walkSpeed = 5f;
+    [SerializeField] private float runSpeed = 8f;
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private float gravity = -9.81f;
+
+
+    [Header("Look")]
     [SerializeField] private float mouseSensivity = 2f;
     [SerializeField] private Transform cameraPivot;
   
@@ -27,6 +35,22 @@ public class FPSController : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X") * mouseSensivity;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensivity;
 
-        Debug.Log(mouseX + " " + mouseY);
+        transform.Rotate(Vector3.up * mouseX);
+
+
+        float verticalRotation = -mouseY;
+
+
+        float currentCameraAngle = cameraPivot.localEulerAngles.x;
+        float newAngle = currentCameraAngle + verticalRotation;
+
+
+        if (newAngle < 180) newAngle -= 360;
+        newAngle = Mathf.Clamp(newAngle, -90, 90);
+
+
+
+        cameraPivot.localEulerAngles = new Vector3(newAngle, 0, 0);
     }
+
 }
