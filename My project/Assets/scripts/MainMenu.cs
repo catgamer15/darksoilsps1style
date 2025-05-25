d1651f8dbcfb7ff56673b5b341e71c355b3beb20
RaisaViKto <112816053+catgamer15@users.noreply.github.com>
2025-05-25 14:51:02 +0300

    mainmenu

---
+++ My project/Assets/scripts/MainMenu.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Метод вызывается при нажатии на кнопку "Старт"
+    public void StartGame()
+    {
+        // Замените "GameScene" на название вашей игровой сцены
+        SceneManager.LoadScene("SampleScene");
+    }
+}
